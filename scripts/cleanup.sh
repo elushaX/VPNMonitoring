6864ee25dda206b15fe552ab50bb0daf382ab206
IlyaShurupov <ilusha@workspace.com>
2024-09-06 14:58:29 +0300

    tmp

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,3 +1,4 @@
 sudo systemctl stop /etc/systemd/system/VPNMonitoring.service
 sudo systemctl disable /etc/systemd/system/VPNMonitoring.service
 sudo rm /etc/systemd/system/VPNMonitoring.service
+sudo systemctl daemon-reload
