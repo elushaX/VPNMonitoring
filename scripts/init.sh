4655626ea3292cec97364ca8f495c9d43b192d57
IlyaShurupov <ilusha@workspace.com>
2024-09-06 14:59:36 +0300

    tmp

--- scripts/init.sh
+++ scripts/init.sh
@@ -8,6 +8,6 @@ echo "ExecStart=/bin/bash $wd/Monitoring.sh" >> VPNMonitoring.service
 echo "WorkingDirectory=$wd" >> VPNMonitoring.service
 
 sudo cp ./VPNMonitoring.service /etc/systemd/system/VPNMonitoring.service
-sudo systemctl daemon-restart
+sudo systemctl daemon-reload
 sudo systemctl enable /etc/systemd/system/VPNMonitoring.service
 sudo systemctl start VPNMonitoring.service
