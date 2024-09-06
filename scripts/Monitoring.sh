72837523d7b4867ef45053349560909f08f6da07
IlyaShurupov <ilusha@workspace.com>
2024-09-06 15:37:32 +0300

    tmp

--- scripts/Monitoring.sh
+++ scripts/Monitoring.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
 cd ../rundir/
-python3 ../graphana/startInfluxBackEnd.py >> log
+
+# Define log file name with current timestamp
+LOGFILE="backend_log_$(date +'%Y-%m-%d_%H-%M-%S').log"
+
+# Run the Python script and log output and crashes to the log file
+python3 ../graphana/startInfluxBackEnd.py > "$LOGFILE" 2>&1
+
+# Optional: Notify on crash
+if [ $? -ne 0 ]; then
+    echo "Script crashed. Check $LOGFILE for details."
+fi
