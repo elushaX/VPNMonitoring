bfd05e54fbb4f28d7ecdb69a9cee06811d2fdf5d
IlyaShurupov <ilusha@workspace.com>
2024-09-11 18:40:47 +0300

    tmp

--- scripts/Monitoring.sh
+++ scripts/Monitoring.sh
@@ -1,27 +1,13 @@
 #!/bin/bash
 
+mkdir ../rundir/
 cd ../rundir/
 
-# Define log file name with current timestamp
-LOGFILE="backend_log_$(date +'%Y-%m-%d_%H-%M-%S').log"
+LOGFILE="log_$(date +'%Y-%m-%d_%H-%M-%S').log"
 
-# Run both Python scripts in parallel and log outputs to the same log file
-{
-  echo "Starting influx.py at $(date)"
-  python3 ../graphana/influx.py
-  echo "Finished influx.py at $(date)"
-} >> "$LOGFILE" 2>&1 &
+./../graphana/runBackEnds.sh
 
-{
-  echo "Starting postgres.py at $(date)"
-  python3 ../graphana/postgres.py
-  echo "Finished postgres.py at $(date)"
-} >> "$LOGFILE" 2>&1 &
 
-# Wait for both background jobs to finish
-wait
-
-# Optional: Notify if either script crashed
 if [ $? -ne 0 ]; then
     echo "One of the scripts crashed. Check $LOGFILE for details."
 fi
