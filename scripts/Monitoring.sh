870dd08e2734a4ddb6bb2fc59afcc61c3bc5489f
IlyaShurupov <ilusha@workspace.com>
2024-09-11 18:42:26 +0300

    tmp

--- scripts/Monitoring.sh
+++ scripts/Monitoring.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
-mkdir ../rundir/
-cd ../rundir/
+# Create the directory if it does not exist
+mkdir -p ../rundir/
 
+# Change to the directory
+cd ../rundir/ || { echo "Failed to change directory to ../rundir/"; exit 1; }
+
+# Define the log file with the current timestamp
 LOGFILE="log_$(date +'%Y-%m-%d_%H-%M-%S').log"
 
-./../graphana/runBackEnds.sh
+# Execute the script and redirect both stdout and stderr to the log file
+../graphana/runBackEnds.sh >> "$LOGFILE" 2>&1
 
-
+# Check if the script execution was successful
 if [ $? -ne 0 ]; then
     echo "One of the scripts crashed. Check $LOGFILE for details."
+else
+    echo "Script executed successfully. Log file: $LOGFILE"
 fi
