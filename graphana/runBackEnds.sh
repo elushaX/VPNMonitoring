bc4dae404724541ded43e527649549584bf6b453
IlyaShurupov <ilusha@workspace.com>
2024-09-11 18:49:05 +0300

    tmp

--- graphana/runBackEnds.sh
+++ graphana/runBackEnds.sh
@@ -1,6 +1,6 @@
 
-python3 trackBasicSystemLoad.py &
-python3 trackAllTimeUsers.py &
-python3 trackActiveUsers.py &
+python3 ../graphana/trackBasicSystemLoad.py &
+python3 ../graphana/trackAllTimeUsers.py &
+python3 ../graphana/trackActiveUsers.py &
 
 wait
